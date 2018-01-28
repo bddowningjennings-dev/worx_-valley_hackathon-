34e38035609ed1eac33a1339785433b4b2bdf0fb
bddowningjennings-dev <bddowningjennings.dev@gmail.com>
2018-01-27 17:40:57 -0800

    newer

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,14 @@ const
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(express.static(__dirname + '/public'))
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/public');
+
 
 require('./server/config/routes')(app);
 require('./server/config/mongoose')(DATABASE);
+
+app.get('/worx', (req, res) => res.render('index'))
 
 app.listen(PORT, ()=>console.log(`(server): listening on port ${PORT}...`))
